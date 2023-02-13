4055897c28690271946b5dd0583014e58a9b15a2
George Soares <george.soares509@gmail.com>
2023-02-13 19:44:09 -0300

    Salvando usuários em memória

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from 'http';
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@gmail.com'
+    })
+
+    return res.end('Criação de usuário');
+  }
+
+  return res.end("Hello World!")
+})
+
+server.listen(3333)
